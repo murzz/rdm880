2b8dfe42e6b3b1855d0be809b04c4a31e1ee11c7
murzzz <murzzz@gmail.com>
2022-12-05 00:00:51 +0200

    Enable `Asan` on windows.
    Add `msvc` compiler flags for `Asan` build type.

--- cmake/asan.cmake
+++ cmake/asan.cmake
@@ -13,18 +13,46 @@ else ()
     endif ()
 endif ()
 
-set(CMAKE_C_FLAGS_ASAN
-        "${CMAKE_C_FLAGS_DEBUG} -fsanitize=address -fno-omit-frame-pointer" CACHE STRING
-        "Flags used by the C compiler for Asan build type or configuration." FORCE)
+if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang" OR "${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU")
+    set(CMAKE_C_FLAGS_ASAN
+            "${CMAKE_C_FLAGS_DEBUG} -fsanitize=address -fno-omit-frame-pointer" CACHE STRING
+            "Flags used by the C compiler for Asan build type or configuration." FORCE)
 
-set(CMAKE_CXX_FLAGS_ASAN
-        "${CMAKE_CXX_FLAGS_DEBUG} -fsanitize=address -fno-omit-frame-pointer" CACHE STRING
-        "Flags used by the C++ compiler for Asan build type or configuration." FORCE)
+    set(CMAKE_CXX_FLAGS_ASAN
+            "${CMAKE_CXX_FLAGS_DEBUG} -fsanitize=address -fno-omit-frame-pointer" CACHE STRING
+            "Flags used by the C++ compiler for Asan build type or configuration." FORCE)
 
-set(CMAKE_EXE_LINKER_FLAGS_ASAN
-        "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} -fsanitize=address" CACHE STRING
-        "Linker flags to be used to create executables for Asan build type." FORCE)
+    set(CMAKE_EXE_LINKER_FLAGS_ASAN
+            "${CMAKE_EXE_LINKER_FLAGS_DEBUG} -fsanitize=address" CACHE STRING
+            "Linker flags to be used to create executables for Asan build type." FORCE)
 
-set(CMAKE_SHARED_LINKER_FLAGS_ASAN
-        "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} -fsanitize=address" CACHE STRING
-        "Linker lags to be used to create shared libraries for Asan build type." FORCE)
+    set(CMAKE_SHARED_LINKER_FLAGS_ASAN
+            "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} -fsanitize=address" CACHE STRING
+            "Linker lags to be used to create shared libraries for Asan build type." FORCE)
+
+    set(CMAKE_STATIC_LINKER_FLAGS_ASAN
+            "${CMAKE_STATIC_LINKER_FLAGS_DEBUG}" CACHE STRING
+            "Linker lags to be used to create static libraries for Asan build type." FORCE)
+elseif ("${CMAKE_CXX_COMPILER_ID}" MATCHES "MSVC")
+    set(CMAKE_C_FLAGS_ASAN
+            "${CMAKE_C_FLAGS_DEBUG} /fsanitize=address /Zi" CACHE STRING
+            "Flags used by the C compiler for Asan build type or configuration." FORCE)
+
+    set(CMAKE_CXX_FLAGS_ASAN
+            "${CMAKE_CXX_FLAGS_DEBUG} /fsanitize=address /Zi /DEBUG" CACHE STRING
+            "Flags used by the C++ compiler for Asan build type or configuration." FORCE)
+
+    set(CMAKE_EXE_LINKER_FLAGS_ASAN
+            "${CMAKE_EXE_LINKER_FLAGS_DEBUG}" CACHE STRING
+            "Linker flags to be used to create executables for Asan build type." FORCE)
+
+    set(CMAKE_SHARED_LINKER_FLAGS_ASAN
+            "${CMAKE_SHARED_LINKER_FLAGS_DEBUG}" CACHE STRING
+            "Linker lags to be used to create shared libraries for Asan build type." FORCE)
+
+    set(CMAKE_STATIC_LINKER_FLAGS_ASAN
+            "${CMAKE_STATIC_LINKER_FLAGS_DEBUG}" CACHE STRING
+            "Linker lags to be used to create static libraries for Asan build type." FORCE)
+else ()
+    message(FATAL_ERROR "Asan profile does not support current C++ compiler: ${CMAKE_CXX_COMPILER_ID}")
+endif ()
